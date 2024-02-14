1ae10b546c1887eebb3e0f9792312cf15dd8b434
Aatif-12 <aatifpothiyawale111@gmail.com>
2024-02-14 15:51:16 +0530

    Second commit

--- src/reverseNumber.java
+++ src/reverseNumber.java
@@ -10,14 +10,14 @@ import java.util.Scanner;
 //    }
 //}
 
-public class kirti {
+public class reverseNumber {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
 
         for (int i = n; i > 0; i--) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
     }
 }
