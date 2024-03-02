bbc0678cb2c7583b766dedb45eee09f173d6d5df
Aatif-12 <aatifpothiyawale111@gmail.com>
2024-03-03 01:45:20 +0530

    mgs

---
+++ src/Array/buyandsell.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class buyandsell {
+
+    public static int buyAndSellStocks(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+
+            if (buyPrice < prices[i]) { // profit
+                int Profit = prices[i] - buyPrice; // today profit
+                maxProfit = Math.max(maxProfit, Profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int prices[] = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(buyAndSellStocks(prices));
+    }
+}
